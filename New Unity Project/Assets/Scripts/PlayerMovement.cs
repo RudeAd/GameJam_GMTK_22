f2a0a7e08d4556297b40b7415430907424f08ebd
Gabriel <Gabriel.axelsson@hotmail.se>
2022-07-16 15:44:30 +0200

    Began movement code
    Deleted all blocks and created a early prototype of player hcaractereee

---
+++ New Unity Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("w"))
+        {
+
+        }
+        else if (Input.GetKeyDown("a"))
+        {
+
+        }
+        else if (Input.GetKeyDown("s"))
+        {
+
+        }
+        else if (Input.GetKeyDown("d"))
+        {
+            
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+        }
+    }
+}
