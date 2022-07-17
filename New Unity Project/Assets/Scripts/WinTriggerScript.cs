33309fdddeb4e920ce2007859ef1d70a478d9adc
Gabriel <Gabriel.axelsson@hotmail.se>
2022-07-17 16:03:26 +0200

    WinCondition

---
+++ New Unity Project/Assets/Scripts/WinTriggerScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinTriggerScript : MonoBehaviour
+{
+
+    private GameObject WinCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        WinCanvas = GameObject.Find("WinCanvas");
+
+        WinCanvas.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            WinCanvas.SetActive(true);
+            Time.timeScale = 0;
+            Debug.Log("Won");
+        }
+    }
+}
